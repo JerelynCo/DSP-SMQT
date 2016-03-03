872207764992bb39621a4ca4836ffe38ecdd8ce5
Jerelyn Co <jerelynco@gmail.com>
2016-03-03 09:38:17 +0800

    LOL IVAN MAGBASA KA

--- dsp_smqt/main.cpp
+++ dsp_smqt/main.cpp
@@ -17,46 +17,13 @@ int main() {
   cout << "Create adspImage Object.." << endl;
 
   adspImage blue(origImage.getBlueVector());
-  adspImage green(origImage.getGreenVector());
-  adspImage red(origImage.getRedVector());
   cout << "Calculate SMQT.." << endl;
 
   vector<unsigned int> bluePixelPositions(blue.getPixelValues().size());
   iota(begin(bluePixelPositions), end(bluePixelPositions), 0);
-  vector<unsigned int> greenPixelPositions(green.getPixelValues().size());
-  iota(begin(greenPixelPositions), end(greenPixelPositions), 0);
-  vector<unsigned int> redPixelPositions(red.getPixelValues().size());
-  iota(begin(redPixelPositions), end(redPixelPositions), 0);
 
   blue.calculateSMQT(bluePixelPositions, 8);
-  green.calculateSMQT(greenPixelPositions, 8);
-  red.calculateSMQT(redPixelPositions, 8);
-  vector<double> blueOut, redOut, greenOut;
-  blueOut = blue.getOutputValues();
-  greenOut = green.getOutputValues();
-  redOut = red.getOutputValues();
-  cout << "Calculation Done!! Combining color channels.." << endl;
-  vector<Mat> channels;
-  auto imageSize = origImage.getImage().size();
-  Mat fin_img, matGreen(imageSize, CV_8UC1), matRed(imageSize, CV_8UC1), matBlue(imageSize, CV_8UC1);
-  int counter = 0;
-  for (int i = 0; i < matGreen.rows; i++)
-  {
-    for (int j = 0; j < matGreen.cols; j++)
-    {
-      matGreen.at<unsigned char>(i, j) = greenOut.at(counter);
-      matBlue.at<unsigned char>(i, j) = blueOut.at(counter);
-      matRed.at<unsigned char>(i, j) = redOut.at(counter);
-      counter++;
-    }
-  }
-  channels.push_back(matBlue);
-  channels.push_back(matGreen);
-  channels.push_back(matRed);
-  merge(channels, fin_img);
-  namedWindow("Result", 1);
-  namedWindow("Orig", 1);
-  imshow("Result", fin_img);
+
   imshow("Orig", origImage.getImage());
   waitKey(0);
   return 0;
